1b7092f7ec8ab12d3747545582bc0587cea381f1
Edgar <digarsg@gmail.com>
2016-06-30 15:19:37 -0300

    start create product form

---
+++ app/src/main/java/alugueis/alugueis/ProductFormActivity.java
@@ -0,0 +1,12 @@
+package alugueis.alugueis;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class ProductFormActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
