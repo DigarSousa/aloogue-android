65fb601b19b97fcd44c31d46d0b469072289dcd9
Edgar <digarsg@gmail.com>
2016-08-05 17:36:39 -0300

    startActivity

--- app/src/main/java/alugueis/alugueis/StartActivity.java
+++ app/src/main/java/alugueis/alugueis/StartActivity.java
@@ -3,10 +3,14 @@ package alugueis.alugueis;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class StartActivity extends AppCompatActivity {
     @BindView(R.id.userNameLogin)
@@ -18,13 +22,23 @@ public class StartActivity extends AppCompatActivity {
     @BindView(R.id.enterButton)
     Button enterButton;
 
-    @BindView(R.id.signUpButton)
-    Button signUpButton;
+    @BindView(R.id.signUp)
+    TextView signUpButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.start_activity);
         ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.enterButton)
+    public void enter(View view) {
+
+    }
+
+    @OnClick(R.id.signUp)
+    public void createAccount(View view) {
+
     }
 }
