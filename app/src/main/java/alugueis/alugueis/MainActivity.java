cc4feb993cd64c5374e4d5f2a9c582b9a2c7bb82
Edgar <digarsg@gmail.com>
2016-08-09 14:36:10 -0300

    locationSettingsRequest dialog

--- app/src/main/java/alugueis/alugueis/MainActivity.java
+++ app/src/main/java/alugueis/alugueis/MainActivity.java
@@ -6,35 +6,14 @@ import alugueis.alugueis.dialogs.PermissionsDialog;
 import alugueis.alugueis.util.MapsUtil;
 
 import android.content.pm.PackageManager;
-import android.os.Bundle;
 
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.util.Log;
+
 import android.view.MenuItem;
 
 public class MainActivity extends DrawerActivity implements PermissionsDialog.PermissionDialogListener {
     private static final String TAG = "MainActivity";
     private MapFragmentView mapFragmentView;
-
-    @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        if (!MapsUtil.isPermissionGranted(this) && savedInstanceState != null) {
-            savedInstanceState.clear();
-            Log.i(TAG, "save state clear");
-        }
-        super.onCreate(savedInstanceState);
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        if (!MapsUtil.isPermissionGranted(this)) {
-            MapsUtil.requestLocationPermition(this);
-        } else {
-            mapFragmentView.startLocationSettings();
-        }
-    }
 
     @Override
     public StandardFragment startFragment() {
@@ -62,7 +41,6 @@ public class MainActivity extends DrawerActivity implements PermissionsDialog.Pe
         }
     }
 
-
     @Override
     public void onPositiveClick(PermissionsDialog dialog) {
         if (!MapsUtil.souldShowRequest(this)) {
@@ -70,10 +48,5 @@ public class MainActivity extends DrawerActivity implements PermissionsDialog.Pe
         } else {
             MapsUtil.requestLocationPermition(this);
         }
-    }
-
-    @Override
-    public void onNegativeClick(PermissionsDialog dialog) {
-        this.finish();
     }
 }
