8d634a48a788939fbbe83430e693d9026739ccd7
Edgar <digarsg@gmail.com>
2016-08-12 09:34:56 -0300

    Range Bar

---
+++ app/src/main/java/alugueis/alugueis/rangebar/RangeBar.java
@@ -0,0 +1,40 @@
+package alugueis.alugueis.rangebar;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.View;
+
+import alugueis.alugueis.R;
+
+
+public class RangeBar extends View {
+    private Paint barPaint;
+
+    public RangeBar(Context context) {
+        super(context);
+        init();
+    }
+
+    public RangeBar(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public RangeBar(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    private void init() {
+        barPaint = new Paint();
+        barPaint.setColor(Color.BLACK);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        canvas.drawRect(100, 200, 100, 200, barPaint);
+    }
+}
